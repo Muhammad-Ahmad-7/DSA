f63ea9ddd59d6889a421abdb092f2b1229ffd6a6
Muhammad-Ahmad-7 <2003ahmadwaheed@gmail.com>
2023-10-25 15:45:47 +0500

    Circular Linked List

--- CircularLinkedList/src/CircularList.java
+++ CircularLinkedList/src/CircularList.java
@@ -1,91 +1,89 @@
 public class CircularList {
+    public Node head = null;
+    public Node lastNode = null;
 
     class Node {
-        int data;
+        int value;
         Node next;
 
-        Node(int data) {
-            this.data = data;
+        Node(int value) {
+            this.value = value;
             this.next = null;
         }
     }
 
-    private Node head;
-
-    public void insert(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            newNode.next = head; // Make it circular
-        } else {
-            Node temp = head;
-            while (temp.next != head) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-            newNode.next = head; // Make it circular
+    public Node lastNode() {
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
         }
+        return node;
     }
 
-    public void delete(int loc) {
+    public void insert(int value) {
         if (head == null) {
-            System.out.println("List is empty. Cannot delete.");
+            Node newNode = new Node(value);
+            head = newNode;
+            newNode.next = newNode;
+            lastNode = head;
             return;
         }
+        Node newNode = new Node(value);
+        newNode.next = head;
+        head = newNode;
+        lastNode.next = head;
+    }
 
-        if (loc == 1) {
-            if (head.next == head) {
-                head = null; // Only one node, remove it
-            } else {
-                Node temp = head;
-                while (temp.next != head) {
-                    temp = temp.next;
-                }
-                temp.next = head.next;
-                head = head.next;
-            }
-        } else {
-            Node current = head;
-            int count = 1;
-            while (count < loc - 1 && current.next != head) {
-                current = current.next;
-                count++;
-            }
-
-            if (count < loc - 1 || current.next == head) {
-                System.out.println("Invalid location: " + loc);
+    public void remove(int data) {
+        Node currentNode = head;
+        Node prevNode = null;
+        if (head.value == data) {
+            head = head.next;
+            lastNode.next = head;
+            return;
+        }
+        do {
+            if (currentNode.value == data) {
+                prevNode.next = currentNode.next;
+                currentNode.next = null;
                 return;
             }
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+        } while (currentNode != head);
 
-            current.next = current.next.next;
-        }
+        // while (currentNode != head) {
+
+        // }
     }
 
-    public void print() {
+    public void display() {
         if (head == null) {
-            System.out.println("Circular Linked List is empty.");
-            return;
+            return; // Handle an empty list
         }
 
-        Node temp = head;
-        do {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        } while (temp != head);
+        Node currentNode = head;
 
-        System.out.println("Head");
+        do {
+            System.out.print(currentNode.value + " -> ");
+            currentNode = currentNode.next;
+        } while (currentNode != head);
+
+        System.out.println(); // Print a new line to format the output
     }
 
     public static void main(String[] args) {
         CircularList cll = new CircularList();
-        cll.insert(1);
-        cll.insert(2);
-        cll.insert(3);
-
-        cll.print();
-
-        // cll.delete(2); // Delete the second element (2)
-
-        // cll.print();
+        cll.insert(5);
+        cll.insert(10);
+        cll.insert(15);
+        cll.insert(20);
+        cll.insert(25);
+        cll.insert(30);
+        cll.display();
+        // cll.remove(5);
+        // cll.display();
+        cll.remove(30);
+        cll.display();
     }
 }
